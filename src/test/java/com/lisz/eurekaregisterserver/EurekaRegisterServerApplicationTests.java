890f5cf5fc812500e1b18ba90dbf0264b5ec0f2e
lisz1012 <lisz1012@163.com>
2022-03-08 21:08:39 -0800

    Initial commit

---
+++ src/test/java/com/lisz/eurekaregisterserver/EurekaRegisterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisz.eurekaregisterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegisterServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

16b4b23df2d32af21718011d5b77b989de5fe17e
lisz1012 <lisz1012@163.com>
2022-03-13 15:31:01 -0700

    Init

---
+++ src/main/java/com/lisz/EurekaServiceProvider.java
@@ -0,0 +1,15 @@
+package com.lisz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EurekaServiceProvider {
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaServiceProvider.class, args);
+    }
+}
